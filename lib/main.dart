f45e962d33ab697960963f10bff5a8538adb1376
sultanularefin <sultanul.arefin@softbakso.com>
2019-04-24 17:56:21 +0600

    dart learning

--- lib/main.dart
+++ lib/main.dart
@@ -8,11 +8,11 @@ class MyApp extends StatelessWidget {
 
 
   Widget build(BuildContext context) {
-   // final wordPair = WordPair.random();
+    // final wordPair = WordPair.random();
     return MaterialApp(
 //      title: 'Welcome to Flutter',
       title: 'Startup Name Generator',
-        home: RandomWords(),
+      home: RandomWords(),
 
 
 //      home: Scaffold(
@@ -25,56 +25,76 @@ class MyApp extends StatelessWidget {
 //          child2: Text('Hello World'),
 
 //          child3: Text('Hello World'),
-         // child: Text(wordPair.asPascalCase),
+      // child: Text(wordPair.asPascalCase),
 //          child: RandomWords(),
 
 
 //        )
 //      ,
 
-
-
     );
   }
-
-
-
-
 }
-
-
 
 
 class RandomWordsState extends State<RandomWords> {
   // TODO Add build() method
 //  @override
 //  Widget build(BuildContext context) {
-   // final wordPair = WordPair.random();
-    final _suggestions = <WordPair>[];
-    final _biggerFont = const TextStyle(fontSize: 18.0);
-    //return Text(wordPair.asPascalCase);
-
-    Widget _buildSuggestions() {
-
-      // ListView is like FlatList of RN
-      return ListView.builder(
-          padding: const EdgeInsets.all(16.0),
-          itemBuilder: /*1*/ (context, i) {
-            if (i.isOdd) return Divider(); /*2*/
-
-            final index = i ~/ 2; /*3*/
-            if (index >= _suggestions.length) {
-              _suggestions.addAll(generateWordPairs().take(10)); /*4*/
-            }
-            return _buildRow(_suggestions[index]);
-          });
-    }
+  // final wordPair = WordPair.random();
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
 
 
+
+  //return Text(wordPair.asPascalCase);
+
+  Widget _buildSuggestions() {
+
+    //debugPrint(_suggestions);
+    //print(_biggerFont);
+
+    // ListView is like FlatList of RN
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: /*1*/ (context, i) {
+         // print('context ==> $context'); // context is not one item as in map(oneItem, index)
+          print('i ==> $i');
+          if (i.isOdd) {
+            print('i is odd --> $i');
+            return Divider();
+          } /*2*/
+
+
+          else{
+
+
+          final index = i ~/ 2; /*3*/
+          if (index >= _suggestions.length) {
+
+            final x = _suggestions.length;
+
+            print('since index ($i) >= _suggestions.length($x)');
+            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+
+            print('_suggestions ==> $_suggestions');
+
+            print('_suggestions is List ==> $_suggestions is list');
+
+
+          }
+          print('_suggestions[index] ==> $_suggestions[$index]');
+          return _buildRow(_suggestions[index]);
+
+          }
+        }
+     // itemCount: litems.length,
+      //  itemCount: 'i',
+
+        );
+  }
 
 //  }
-
-
 
   Widget _buildRow(WordPair pair) {
     return ListTile(
@@ -96,8 +116,8 @@ class RandomWordsState extends State<RandomWords> {
       body: _buildSuggestions(),
     );
   }
-  // #enddocregion RWS-build
-  // #docregion RWS-var
+// #enddocregion RWS-build
+// #docregion RWS-var
 }
 // #enddocregion RWS-var
 
@@ -105,7 +125,7 @@ class RandomWordsState extends State<RandomWords> {
 class RandomWords extends StatefulWidget {
   @override
   RandomWordsState createState() => RandomWordsState();
-  //RandomWordsState createState() => new RandomWordsState();
+//RandomWordsState createState() => new RandomWordsState();
 }
 
 
