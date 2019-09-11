2c0464941929551b407fad67a1813cc4440023bf
arefinmonipur212 <testafinsultan@gmail.com>
2019-09-11 23:58:00 +0600

    commit before checkout

--- lib/main.dart
+++ lib/main.dart
@@ -50,7 +50,10 @@ class _MyHomePageState extends State<MyHomePage> {
     setState(() {
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
+      // so that the display can reflect the updated values.
+      //
+      //
+      // If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
       _counter++;
@@ -76,24 +79,42 @@ class _MyHomePageState extends State<MyHomePage> {
         // in the middle of the parent.
         child: Column(
           // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
+          // arranges them vertically. By default,
           //
+          // it sizes itself to fit its
+          // children horizontally,
+          //
+          // and tries to be as tall as its parent.
+
+
           // Invoke "debug painting" (press "p" in the console, choose the
           // "Toggle Debug Paint" action from the Flutter Inspector in Android
           // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
           // to see the wireframe for each widget.
+
+
           //
           // Column has various properties to control how it sizes itself and
           // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
+          // center the children vertically;
+          //
+          // the main axis here is the vertical
           // axis because Columns are vertical (the cross axis would be
           // horizontal).
+
+
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
               'You have pushed the button this many times:',
             ),
+
+          /// Very large text.
+          // final TextStyle display2;
+
+          /// Large text.
+          //final TextStyle display1;
+            //
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.display1,
